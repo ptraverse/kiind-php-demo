e66c62baa70794fe940ee99faea6c403d45f0fd3
root <philippe.traverse@gmail.com>
2014-10-31 17:36:06 -0700

    started using ow.ly API with Guzzle

--- config.php
+++ config.php
@@ -1,4 +1,9 @@
 <?php
 require 'vendor/autoload.php';
 
+//Define OWLY_API_KEY and OWLY_BASE_URL
+$owly_api_key_file = file_get_contents("./private/owly_api_key.json");
+$owly_api_key_array = json_decode($owly_api_key_file, TRUE);
+define("OWLY_API_KEY",(string)($owly_api_key_array['owly_api_key']));
+define("OWLY_BASE_URL",(string)($owly_api_key_array['base_url']));
 
